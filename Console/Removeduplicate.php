2d0399f773f1e48181a62cb76ae114455d8e9837
Martin Kovachev <miracle@nimasystems.com>
2023-05-16 23:07:21 +0300

    fix: issue with files

--- Console/Removeduplicate.php
+++ Console/Removeduplicate.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Magegadgets\RemoveDuplicateImage\Console;
 
 use Symfony\Component\Console\Command\Command;
@@ -7,47 +8,48 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Removeduplicate extends Command
 {
-   protected function configure()
-   {
-       $this->setName('duplicate:remove');
-       $this->setDescription('Demo command line');
-       
-       parent::configure();
-   }
-   protected function execute(InputInterface $input, OutputInterface $output)
-   {
+    protected function configure()
+    {
+        $this->setName('duplicate:remove');
+        $this->setDescription('Demo command line');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 
         $app_state = $objectManager->get('\Magento\Framework\App\State');
         $app_state->setAreaCode('global');
-        ini_set('memory_limit','10240M');
-        ini_set('max_execution_time', 0); 
+        ini_set('memory_limit', '10240M');
+        ini_set('max_execution_time', 0);
         set_time_limit(0);
         $mediaApi = $objectManager->create('\Magento\Catalog\Model\Product\Gallery\Processor');
         $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
         $storeManager->setCurrentStore(0);
         $mediaUrl = $storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
-        $directoryList=$objectManager->get('\Magento\Framework\App\Filesystem\DirectoryList');
-        $path = $directoryList->getPath('media'); 
-        $productCollection=$objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
+        $directoryList = $objectManager->get('\Magento\Framework\App\Filesystem\DirectoryList');
+        $path = $directoryList->getPath('media');
+        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
         $_products = $productCollection->addAttributeToSelect('*')->load();
 
         $productRepository = $objectManager->get('\Magento\Catalog\Model\ProductRepository');
-        $i =0;
+        $i = 0;
         $total = count($_products);
         $count = 0;
-        $productCount = array();
-        foreach($_products as $_prod) {
+        $productCount = [];
+        foreach ($_products as $_prod) {
             $_product = $productRepository->getById($_prod->getId());
             $_product->setStoreId(0);
-            $_md5_values = array();
+            $_md5_values = [];
             $base_image = $_product->getImage();
-            
-            if($base_image != 'no_selection') {
+
+            if ($base_image != 'no_selection') {
                 $mediaUrl = $storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
-                $filepath = $path.'/catalog/product' . $base_image ;
+                $filepath = $path . '/catalog/product' . $base_image;
                 if (file_exists($filepath)) {
-                    $_md5_values[] = md5(file_get_contents($filepath));            
+                    $_md5_values[] = md5(file_get_contents($filepath));
                 }
                 $i++;
 
@@ -57,36 +59,36 @@ class Removeduplicate extends Command
                 if ($gallery) {
                     foreach ($gallery as $key => $galleryImage) {
                         //protected base image
-                        if($galleryImage->getFile() == $base_image) {
+                        if ($galleryImage->getFile() == $base_image) {
                             continue;
                         }
-                        $filepath = $path.'/catalog/product' .$galleryImage->getFile();
+                        $filepath = $path . '/catalog/product' . $galleryImage->getFile();
 
-                        if(file_exists($filepath)) {
+                        if (file_exists($filepath) && is_file($filepath)) {
                             $md5 = md5(file_get_contents($filepath));
                         } else {
                             continue;
                         }
 
-                        if( in_array( $md5, $_md5_values )) {
+                        if (in_array($md5, $_md5_values)) {
                             if (count($galleryImage->getTypes()) > 0) {
                                 continue;
                             }
                             unset($gallery[$key]);
-                            echo "\r\n removed duplicate image from ".$_product->getSku();
+                            echo "\r\n removed duplicate image from " . $_product->getSku();
                             $count++;
                         } else {
                             $_md5_values[] = $md5;
                         }
                     }
-                    
+
                     $_product->setMediaGalleryEntries($gallery);
-                    $productRepository->save($_product);            
+                    $productRepository->save($_product);
                 }
                 //$_product->save();
             }
-            }
+        }
 
-               $output->writeln("\r\n Duplicate Images are Removed");
-           }
+        $output->writeln("\r\n Duplicate Images are Removed");
+    }
 }
